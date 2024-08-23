eb510aade48bae02e6e6679766b8ab6d9fe28048
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-08-23 10:47:04 +0200

    setup mixin variables files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import styles from "./page.module.css";
+import Todo from "@/components/Todo";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <p>The sassy-do</p>
-    </main>
+      <Todo />
   );
 }
---
+++ src/components/Todo/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div className='main'>TodoList</div>
+  )
+}
+
+export default Todo
